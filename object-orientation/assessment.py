0d5f5d4fd5c7e4fdaa554b336a456a11e1e44913
Katie <knt.taylor@gmail.com>
2017-03-04 20:54:23 +0000

    added ask_and_evaluate method to Question class and add_questions method to Exam class

--- object-orientation/assessment.py
+++ object-orientation/assessment.py
@@ -23,6 +23,7 @@ class Student(object):
     """Creates a new student and assigns a name and address."""
 
     def __init__(self, first_name, last_name, address):
+        """Initialize student attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
@@ -32,13 +33,31 @@ class Question(object):
     """Creates questions and answers"""
 
     def __init__(self, question, correct_answer):
+        """Initialize question attributes."""
+
         self.question = question
         self.correct_answer = correct_answer
+
+    def ask_and_evaluate(self):
+        """ ."""
+
+        print self.question
+        self.user_answer = raw_input()
+
+        return self.correct_answer.lower() == self.user_answer.lower()
 
 
 class Exam(object):
     """Creates the exam with a list of questions"""
 
     def __init__(self, name):
+        """Initialize exam attributes."""
+
         self.name = name
         self.questions = []
+
+    def add_question(self, question, correct_answer):
+        """Adds question and answer instances to the list of questions"""
+
+        self.question = Question(question, correct_answer)
+        self.questions.append(self.question)
