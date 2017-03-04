25149527928d2aed7df956f3d0cf74cf771fb5d8
Katie <knt.taylor@gmail.com>
2017-03-04 17:32:45 +0000

    implements find_unique_common_items()

--- dictionaries/practice.py
+++ dictionaries/practice.py
@@ -61,8 +61,8 @@ def find_unique_common_items(items1, items2):
         >>> sorted(find_unique_common_items(["2", "1", 2], [2, 1]))
         [2]
     """
-
-    return []
+    unique_items = list(set(items1) & set(items2))
+    return unique_items
 
 def get_sum_zero_pairs(numbers):
     """Given list of numbers, return list of pairs summing to 0.
