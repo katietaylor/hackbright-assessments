e51029045ef93477b50a73f1229c6abb6a5000f0
Katie <knt.taylor@gmail.com>
2017-04-15 19:28:51 +0000

    completed breath first search

--- cs-data-struct-1/tree.py
+++ cs-data-struct-1/tree.py
@@ -26,9 +26,7 @@ class Node(object):
             2
         """
 
-        # FIXME
-
-        pass
+        return len(self.children)
 
 
 class Tree(object):
@@ -57,7 +55,6 @@ class Tree(object):
                 return node
 
             to_visit.extend(node.children)
-
 
     def breadth_first_search(self, data):
         """Return node object with this data, traversing the tree breadth-first.
@@ -93,8 +90,15 @@ class Tree(object):
 
         """
 
-        # FIXME
-        pass
+        to_visit = [self.root]
+
+        while to_visit:
+            node = to_visit.pop(0)
+
+            if node.data == data:
+                return node
+
+            to_visit.extend(node.children)
 
 if __name__ == "__main__":
     import doctest
